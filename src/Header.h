4a0c5b40c331e317839566ec79072c8e0eec750f
limdh <limdenghong@hotmail.com>
2018-04-03 17:55:37 +0800

    Resolved merge conflict to add mean and median feature.

--- src/Header.h
+++ src/Header.h
@@ -5,15 +5,7 @@
 #include <cmath>
 
 void bubblesort(int(&dataset)[5]);
-<<<<<<< HEAD
-
-
-
-#endif
-
-=======
 float getMean(int dataset[5]);
 float getMedian(int dataset[5]);
 
 #endif
->>>>>>> Resolved merge conflict to add mean and median feature.
