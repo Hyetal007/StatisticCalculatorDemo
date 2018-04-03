2cfeedadeca00d8b325f901ed9201c00c79daecd
Thomas Liem <hyetal007@gmail.com>
2018-04-03 18:44:23 +0800

    Solved rebase conflict

--- src/Header.h
+++ src/Header.h
@@ -4,14 +4,12 @@
 #include <iostream>
 #include <cmath>
 
-<<<<<<< 7af350586ec5cff7c35527d6c988a2219f6360fa
+using namespace std;
+
 void bubblesort(int(&dataset)[5]);
 float getMean(int dataset[5]);
 float getMedian(int dataset[5]);
 float getMode(int dataset[5]);
 float getStd(int dataset[5], float mean);
-=======
-using namespace std;
->>>>>>> Added namespace
 
 #endif
